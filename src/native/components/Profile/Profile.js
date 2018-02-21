bcf1e1b8c853d15a5b5731b337af4af43d2f6017
Emelie Javelind <emelie@auddly.com>
2018-02-20 16:12:46 -0800

    Added share functionality with mock link/message

--- src/native/components/Profile/Profile.js
+++ src/native/components/Profile/Profile.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View, Container, Content, List, ListItem, Body, Left, Text, Icon } from 'native-base';
+import { Platform, Share } from 'react-native';
+import { View, Container, Content, List, ListItem, Body, Left, Text, Icon, Fab } from 'native-base';
 import { Actions } from 'react-native-router-flux';
 
 import Header from '../Header';
 import Cover from './Cover';
 import TypeList from './TypeList';
+
+const onShare = () => {
+  const url = 'https://aftonbladet.se';
+  const title = 'This is a title';
+  const message = 'This is a message';
+  const displayMessage = Platform.OS === 'ios' ? message : `${message} ${url}`;
+
+  Share.share({
+    title,
+    message: displayMessage,
+
+    // iOS
+    url,
+    subject: title,
+
+    // Android
+    dialogTitle: title,
+  });
+};
 
 const Profile = ({
   member,
@@ -63,6 +83,26 @@ const Profile = ({
         </List>
       }
     </Content>
+
+    {loggedIn && (
+      <Fab
+        position="bottomRight"
+        style={{ backgroundColor: '#473BE7' }}
+        direction="up"
+        containerStyle={{ }}
+        onPress={onShare}
+      >
+        {/* Brand background color */}
+        <Icon ios="ios-share" android="md-share" style={{ fontSize: 30, color: 'white' }} />
+
+        {/* TouchableOpacity as child causes warning for invalid view color key */}
+        {/*
+          <TouchableOpacity onPress={onShare} >
+            <Icon ios="ios-share" android="md-share" style={{ fontSize: 30, color: 'white' }} />
+          </TouchableOpacity>
+        */}
+      </Fab>
+    )}
   </Container>
 );
 
