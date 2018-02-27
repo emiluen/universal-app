70e5bb15865dae8cb6dd72199298b027c4d0d9d0
Emelie Javelind <emelie@auddly.com>
2018-02-26 18:10:06 -0800

    Type stying web

--- src/web/components/Personalities/Type.js
+++ src/web/components/Personalities/Type.js
@@ -10,12 +10,13 @@ import {
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-import { TemplateContainer } from '../Templates/Templates';
+import { TemplateWrapper, TemplateContainer } from '../Templates/Templates';
 import ErrorMessages from '../../../constants/errors';
 import Loading from '../Loading';
 import Error from '../Error';
 import ArticleContainer from '../../../containers/Article';
 import ArticleComponent from './Article';
+import getImageUrl from '../../../selectors/get-image-url';
 
 class TypeView extends React.Component {
   onAddPersonality = () => this.props.addPersonality(this.props.personality, this.props.type.id);
@@ -41,45 +42,49 @@ class TypeView extends React.Component {
     if (!type) return <Error content={ErrorMessages.recipe404} />;
 
     return (
-      <TemplateContainer>
-        <Row>
-          <Col sm="12">
-            <h1>{type.name} - {type.nickname}</h1>
-          </Col>
-          <Col sm="12">
-            {canAddPersonality ?
-              <Button onClick={this.onAddPersonality}>
-                Add to my profile
-              </Button>
-              : null
-            }
-            {canRemovePersonality ?
-              <Button onClick={this.onRemovePersonality}>
-                Remove from my profile
-              </Button>
-              : null
-            }
-          </Col>
-        </Row>
-        <Row>
-          <Col lg="4" className="recipe-view-card">
-            <Card>
-              <CardHeader>Group</CardHeader>
-              <CardBody>
-                <ArticleContainer
-                  Layout={ArticleComponent}
-                  article={type.article}
-                />
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
-        <Row className="pb-3">
-          <Col sm="12">
-            <Link className="btn btn-secondary" to={`/personalities/${personality.id}`}><i className="icon-arrow-left" /> Back</Link>
-          </Col>
-        </Row>
-      </TemplateContainer>
+      <div>
+        <div style={{ height: 200 }}>
+          <img src={getImageUrl(type.coverImageUrl, { width: 1200 })} style={{ height: 200, width: '100%', objectFit: 'cover' }} />
+        </div>
+        <TemplateContainer>
+          <Row>
+            <Col xs="12" md="8">
+              <h1>{type.name} - {type.nickname}</h1>
+            </Col>
+            <Col xs="12" md="4">
+              {canAddPersonality ?
+                <Button color="primary" onClick={this.onAddPersonality} className="float-md-right">
+                  Add to my profile
+                </Button>
+                : null
+              }
+              {canRemovePersonality ?
+                <Button outline color="primary" onClick={this.onRemovePersonality} className="float-md-right">
+                  Remove from my profile
+                </Button>
+                : null
+              }
+            </Col>
+          </Row>
+          <Row>
+            <Col sm="12" className="recipe-view-card">
+              <Card>
+                <CardBody>
+                  <ArticleContainer
+                    Layout={ArticleComponent}
+                    article={type.article}
+                  />
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+          <Row className="pb-3">
+            <Col sm="12">
+              <Link className="btn btn-secondary" to={`/personalities/${personality.id}`}><i className="icon-arrow-left" /> Back</Link>
+            </Col>
+          </Row>
+        </TemplateContainer>
+      </div>
     );
   }
 }
